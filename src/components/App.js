d0b8ed7d9d959d2cd13d35985fea339b75f1a452
jafar <mirzaraximovjafar2001@gmail.com>
2022-04-27 09:25:28 +0500

    create buttons Components

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,22 @@ import React from "react";
 import axios from "axios";
 import SearchBar from "./SearchBar";
 import ImageList from "./ImageList";
+import Buttons from "./Buttons";
 // import SearchBar from "./SearchBar";
 
 class App extends React.Component {
-  state = { imageArr: [] };
+  state = { imageArr: [], search: "", page: 1 };
 
-  SearchdanMalumotniOl = async (imgdata) => {
+  SearchdanMalumotniOl = async (imgdata, pg = 1) => {
+    if (pg) {
+      this.setState({ page: pg });
+      this.setState({ search: imgdata });
+    }
     const data = await axios.get("https://api.unsplash.com/search/photos", {
       params: {
         query: imgdata,
-        page: 1,
-        per_page: 1000,
+        page: pg,
+        per_page: 20,
       },
       headers: {
         Authorization: "Client-ID fFxqUKuJS8hx3nwzRQoYiHB5URcqFjkGG-FSsZUDOb4",
@@ -24,6 +29,11 @@ class App extends React.Component {
   render() {
     return (
       <div>
+        <Buttons
+          func={this.SearchdanMalumotniOl}
+          search={this.state.search}
+          page={this.state.page}
+        />
         <SearchBar malumotniOl={this.SearchdanMalumotniOl} />
         <ImageList getData={this.state.imageArr} />
       </div>
---
+++ src/components/Buttons.js
@@ -0,0 +1,22 @@
+import React from "react";
+
+function Buttons(props) {
+  const orqaga = () => {
+    props.func(props.search, props.page - 1);
+  };
+  const oldinga = () => {
+    props.func(props.search, props.page + 1);
+  };
+  return (
+    <div className="buttons">
+      <button onClick={orqaga} className="prev_btn positive ui button">
+        Prev
+      </button>
+      <button onClick={oldinga} className="next_btn negative ui button">
+        Next
+      </button>
+    </div>
+  );
+}
+
+export default Buttons;
